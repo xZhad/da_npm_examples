9749897d8a7c2be64c11bcf2a43eb327b6f27650
Ivan Sarpi <idsarpiu@falabella.cl>
2019-12-19 11:02:12 -0300

    axios promises all

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ const get = async () => {
     });
 };
 
+const all = async () => {
+  const proms = [];
+  for (let i = 1; i <= 25; i += 1) {
+    proms.push(axios.get(`https://rickandmortyapi.com/api/character/?page=${i}`));
+  }
+  const res = await Promise.all(proms);
+  const datas = res.map((r) => r.data.results);
+  const x = [];
+  for (let i = 0; i < datas.length; i += 1) {
+    x.push(...datas[i]);
+  }
+  console.log(x);
+};
+
 get();
+all();
